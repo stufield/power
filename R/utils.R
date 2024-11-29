d2c5eabc413efc04a477b6bec22e5beddbf57b95
Stu Field <stu.g.field@gmail.com>
2024-11-29 12:43:45 -0700

    Change color palette

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,17 @@
 
-col_string <- c("dodgerblue", "red", "darkgreen",
-                "darkorchid4", "cyan", "orange",
-                "black", "grey", "#990066", "green", "#24135F")
+col_string <- c(
+  purple     = "#24135F",
+  lightgreen = "#00A499",
+  lightgrey = "#707372",
+  magenta   = "#840B55",
+  lightblue = "#006BA6",
+  yellow    = "#D69A2D",
+  darkgreen = "#007A53",
+  darkblue  = "#1B365D",
+  darkgrey  = "#54585A",
+  blue = "#004C97"
+)
+
 
 par_def <- list(mgp = c(2.00, 0.75, 0.00), mar = c(3, 4, 3, 1))
 
