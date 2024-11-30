7eefd4a290d7bc04ac048313d75c997070b88b0b
Stu Field <stu.g.field@gmail.com>
2024-11-30 14:13:44 -0700

    Fix argument collision in solve_n()
    - the `pwr_tbl` and `pwr` params were colliding
      in the S3 methods
    - change `pwr_tbl` -> `x`

--- R/solve-n.R
+++ R/solve-n.R
@@ -6,10 +6,10 @@
 #'   efficiently returns the sample size
 #'   for a given power.
 #'
-#' @param pwr_tbl A `pwr_tbl` object.
+#' @param x A "power_curve" object. See [fisher_power_curve()]
+#'   or [t_power_curve()].
 #' @param pwr `double(1)` in \verb{(0, 1)}.
 #'   The power to be interpolated.
-#' @importFrom stats loess
 #' @examples
 #' tbl <- fisher_power_curve(seq(50, 400, 25))
 #' solve_n(tbl, 0.85)
@@ -18,40 +18,42 @@
 #'
 #' solve_n(tbl, 0.55)
 #' @export
-solve_n <- function(pwr_tbl, pwr) {
+solve_n <- function(x, pwr) {
   UseMethod("solve_n")
 }
 
 #' @noRd
 #' @importFrom helpr value
 #' @export
-solve_n.default <- function(pwr_tbl, pwr) {
+solve_n.default <- function(x, pwr) {
   stop(
     "Could not determine S3 `solve_n()` method for class: ",
-    value(class(pwr_tbl)),
+    value(class(x)),
     call. = FALSE
   )
 }
 
+# This is the primary method
+# A tibble with `n` and `power` columns
 #' @noRd
+#' @importFrom stats loess
 #' @export
-solve_n.fisher_power_curve <- function(pwr_tbl, pwr) {
-  fit <- loess(power ~ n, data = pwr_tbl)
+solve_n.tbl_df <- function(x, pwr) {
+  fit <- loess(power ~ n, data = x)
   min_pwr <- min(fit$fitted)
   max_pwr <- max(fit$fitted)
   if ( pwr < min_pwr || pwr > max_pwr ) {
     stop("`pwr` is outside of interpolated range: ", pwr, call. = FALSE)
   }
-  # objective function
-  fn <- function(x) {
+  fn <- function(x) {  # objective function
     y <- unname(predict(fit, data.frame(n = x)))
     alpha <- (y - pwr) / (1 - pwr)  # proportion of range above pwr
-    # alpha < 0 if y is below pwr (return y)
-    # if alpha > 0, threshold y and decrease by proportion above pwr (alpha)
+    # alpha < 0 if y is below pwr; return y
+    # alpha > 0; threshold y and decrease by proportion above pwr (alpha)
     # creates a peaked objective function at pwr to maximize below
     ifelse(alpha > 0, pwr - (pwr * alpha), y)
   }
-  #approx(x = fit$fitted, y = pwr_tbl$n, xout = pwr) |> unlist()
+  #approx(x = fit$fitted, y = x$n, xout = pwr) |> unlist()
   range <- range(as.numeric(fit$x))
   opt <- optimize(fn, interval = range, maximum = TRUE, tol = 0.001)
   round(c(power = opt$objective, n = opt$maximum), 3L)
@@ -59,31 +61,30 @@ solve_n.fisher_power_curve <- function(pwr_tbl, pwr) {
 
 #' @noRd
 #' @export
-solve_n.t_power_curve <- function(pwr_tbl, pwr) {
+solve_n.fisher_power_curve <- function(x, pwr) {
   stopifnot(
-    "Power curve object must vary `n` to solve for it." =
-      inherits(pwr_tbl, "class_n")
+    "Sanity check ... `n` and `power` must be in the tibble!" =
+      all(c("n", "power") %in% names(x))
   )
   NextMethod()
 }
 
 #' @noRd
 #' @export
-solve_n.class_n <- function(pwr_tbl, pwr) {
-  pwr_tbl <- vapply(pwr_tbl$tbl, stats::median, double(1)) |>
-    tibble::enframe(name = "n", value = "power")
-  fit <- loess(power ~ n, data = pwr_tbl)
-  min_pwr <- min(fit$fitted)
-  max_pwr <- max(fit$fitted)
-  if ( pwr < min_pwr || pwr > max_pwr ) {
-    stop("`pwr` is outside of interpolated range: ", pwr, call. = FALSE)
-  }
-  fn <- function(x) {
-    y <- unname(predict(fit, data.frame(n = x)))
-    alpha <- (y - pwr) / (1 - pwr)  # proportion of range above pwr
-    ifelse(alpha > 0, pwr - (pwr * alpha), y)
-  }
-  range <- range(as.numeric(fit$x))
-  opt <- optimize(fn, interval = range, maximum = TRUE, tol = 0.001)
-  round(c(power = opt$objective, n = opt$maximum), 3L)
+solve_n.t_power_curve <- function(x, pwr) {
+  # Stop to ensure you're not acting on a `class_delta` object
+  stopifnot(
+    "Power curve object must vary `n` to solve for it." = inherits(x, "class_n")
+  )
+  NextMethod()
+}
+
+#' @noRd
+#' @importFrom tibble enframe
+#' @importFrom stats median
+#' @export
+solve_n.class_n <- function(x, pwr) {
+  tbl <- vapply(x$tbl, stats::median, double(1)) |>
+    enframe(name = "n", value = "power")
+  solve_n(tbl, pwr = pwr)
 }
